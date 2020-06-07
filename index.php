471e070a08bbe2d89d98c590b20a8699dfa48230
Pheonix0821 <56611945+Pheonix0821@users.noreply.github.com>
2020-06-06 19:51:59 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Volcan Share</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="headerMenu">
+    <div id="wrapper">
+        <div class="logo">
+<img src="./img/VolcanShareLogo.png">
+        </div>
+        <div class="search_box">
+            <form action="search.php" method="GET" id="search">
+                <input type="text" name="q" size="60" placeholder="search..." />
+            </form>
+        </div>
+        <div id="menu">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Register</a>
+            <a href="#">Login</a>
+            <a href="#">?</a>
+            <a href="#">??</a>
+        </div>
+    </div>
+</div>
+<div style="width: 1000px; margin: 0px auto 0px auto;">
+<table id="werty">
+    <tr>
+        <td width="60%" vallign="top"> 
+<h2>Register for volcan Share </h2>
+        </td>
+        <td width="40%" vallign="top">
+<h2>Sign up below</h2>
+<form action="#" method="POST">
+<input type="text" name="fname" size="25" placeholder="first name"><br /><br />
+<input type="text" name="lname" size="25" placeholder="last name"><br /><br />
+<input type="text" name="username" size="25" placeholder="username"><br /><br />
+<input type="text" name="email" size="25" placeholder="email"><br /><br />
+<input type="text" name="email2" size="25" placeholder="email (again)"><br /><br />
+<input type="text" name="password" size="25" placeholder="password"><br /><br />
+<input type="text" name="password2" size="25" placeholder="retype password"><br /><br />
+<input type="submit" name="submit" value="Register">
+
+
+</form>
+        </td>
+    </tr>
+</table>
+</div>
+</body>
+</html>
